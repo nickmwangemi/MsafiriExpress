b8451286e0f4e5cbdbdcef70f4c53eda24ab59ce
Nick Mwangemi <nickmwangemi@gmail.com>
2020-05-13 12:09:39 +0300

    Changed DEBUG to False in prod environment

--- Msafiri/settings.py
+++ Msafiri/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '*nzt4fpbi4^(!refangzopuq&8xb8v
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = 'False'
+DEBUG = os.environ.get('DJANGO_DEBUG', 'False') != 'False'
 
 ALLOWED_HOSTS = ['127.0.0.1', 'msafiri.loclx.io','e1d626d8.ngrok.io','msafiriexpress.herokuapp.com']
 
